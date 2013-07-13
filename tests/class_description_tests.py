d685d140b5b1c84055679de9744e9f57be94475f
Dave Shawley <daveshawley@gmail.com>
2013-07-13 10:07:46 -0400

    Make test names readable.

--- tests/class_description_tests.py
+++ tests/class_description_tests.py
@@ -3,47 +3,47 @@ import unittest
 from fluenttest import ClassTester, the_class
 
 
-class TheClassFunctionShould(unittest.TestCase):
+class TheClassFunction(unittest.TestCase):
 
-    def test_be_extensible(self):
+    def should_be_extensible(self):
         describer = the_class(dict, matcher_class=_ExtendedClassTester)
         assert isinstance(describer, _ExtendedClassTester)
 
 
-class ImplementMethodsShould(unittest.TestCase):
+class ImplementMethods(unittest.TestCase):
 
     def setUp(self):
         self.describer = the_class(dict)
 
-    def test_returns_True_for_existing_method(self):
+    def should_return_True_for_existing_method(self):
         assert self.describer.implements_method('setdefault')
 
-    def tests_return_False_for_missing_methods(self):
+    def should_return_False_for_missing_methods(self):
         assert not self.describer.implements_method('missing_method')
 
 
-class IsSubclassOfShould(unittest.TestCase):
+class IsSubclassOf(unittest.TestCase):
 
     def setUp(self):
         self.describer = the_class(_ClassUnderTest)
 
-    def test_returns_True_for_parent_class(self):
+    def should_return_True_for_parent_class(self):
         assert self.describer.is_subclass_of(_ParentClass)
 
-    def test_accept_class_as_fully_qualified_path_name(self):
+    def should_accept_class_as_fully_qualified_path_name(self):
         assert self.describer.is_subclass_of(
             'tests.class_description_tests._GrandparentClass')
 
 
-class HasAttributeShould(unittest.TestCase):
+class HasAttribute(unittest.TestCase):
 
     def setUp(self):
         self.describer = the_class(_ClassUnderTest)
 
-    def test_returns_True_for_class_attribute(self):
+    def should_return_True_for_class_attribute(self):
         assert self.describer.has_attribute('class_attribute')
 
-    def test_can_check_attribute_type(self):
+    def should_can_check_attribute_type(self):
         assert self.describer.has_attribute('class_attribute', typed=basestring)
 
 
--- tests/helper_tests.py
+++ tests/helper_tests.py
@@ -3,15 +3,15 @@ import unittest
 from fluenttest import lookup_class
 
 
-class LookupClassShould(unittest.TestCase):
+class LookupClass(unittest.TestCase):
 
-    def test_return_class_when_given_class_as_argument(self):
+    def should_return_class_when_given_class_as_argument(self):
         assert lookup_class(dict) is dict
 
-    def test_import_class_by_absolute_path(self):
+    def should_import_class_by_absolute_path(self):
         assert lookup_class('unittest.TestCase') is unittest.TestCase
 
-    def test_raise_AssertionError_when_given_something_else(self):
+    def should_raise_AssertionError_when_given_something_else(self):
         assertion_caught = False
         try:
             lookup_class(1234)
