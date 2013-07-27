23faf9848abfed9838b6e5537113065c830373e3
Dave Shawley <daveshawley@gmail.com>
2013-07-26 20:55:38 -0400

    Don't include package versions in requirements.
    After reading Donald Stufft's blog post [setup.py versus
    requirements.txt](https://caremad.io/blog/setup-vs-requirements/), I
    decided that stripping the version numbers is just the right thing to
    do.  Coincidentally, I ran into some problems with Sentry requiring
    Jinja2==2.7 when all of the other requirements and our distribution just
    happened to be using 2.6.  Anyway, read what dstufft has to say about it
    and stop including version numbers in your setup.py!

--- setupext.py
+++ setupext.py
@@ -1,7 +1,14 @@
+from operator import attrgetter
+
 from pkg_resources import parse_requirements
 
 
-def read_requirements_from_file(file_path):
+def read_requirements_from_file(file_path, include_versions=False):
+    if include_versions:
+        to_string = str
+    else:
+        to_string = attrgetter('project_name')
+
     handle = None
     requirements = []
     try:
@@ -10,7 +17,7 @@ def read_requirements_from_file(file_path):
         for line in handle:
             try:
                 for req in parse_requirements([line]):
-                    requirements.append(str(req))
+                    requirements.append(to_string(req))
             except ValueError as exc:
                 print('Warning: {0} ({1}:{2})'.format(
                     ' '.join(exc.args), file_path, line_no))
